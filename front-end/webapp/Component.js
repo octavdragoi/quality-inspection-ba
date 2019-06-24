1bd02a1b30771b5e80625a3b417a524b17df7395
octavdragoi <octav.dragoi@tum.de>
2019-06-24 18:12:04 +0000

    add  front-end

---
+++ front-end/webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function (AppComponent) {
+	return AppComponent.extend("front-end.Component", {
+		metadata: {
+			"manifest": "json"
+		}
+	});
+});
